6f863265e352ac6f1d9a362098771e3cfd0cc44c
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2021-12-02 17:10:02 -0500

    first commit

---
+++ TypedDataSetExample/CrudOperationsTypedDataSets.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypedDataSetExample
+{
+    class CrudOperationsTypedDataSets
+    {
+        private DataSetTemplateTableAdapters.ProductsTableAdapter productsTableAdapter;
+        private DataSetTemplate.ProductsDataTable dataTable;
+
+        internal CrudOperationsTypedDataSets()
+        {
+            productsTableAdapter = new DataSetTemplateTableAdapters.ProductsTableAdapter();
+            dataTable = new DataSetTemplate.ProductsDataTable();
+        }
+
+        public void GetAllProducts()
+        {
+            //productsTableAdapter.Fill(dataTable); or other way is
+            dataTable = productsTableAdapter.GetProducts();
+            foreach(var row in dataTable)
+            {
+                Console.WriteLine($"{row.ProductID} {row.ProductName}");
+            }
+        }
+
+        public void GetProductById(int id)
+        {
+            dataTable = productsTableAdapter.GetProducts();
+            var row = dataTable.FindByProductID(id);
+            if(row != null)
+            {
+                Console.WriteLine($"{row.ProductID} {row.ProductName} {row.UnitPrice} {row.UnitsInStock}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid ID");
+            }
+        }
+
+        public void GetProductByName(string name)
+        {
+            dataTable = productsTableAdapter.GetProductByName(name);
+            if(dataTable.Count > 0)
+            {
+                var row = dataTable[0];
+                Console.WriteLine($"{row.ProductID} {row.ProductName} {row.UnitPrice} {row.UnitsInStock}");
+            }
+        }
+
+        public void InsertProduct(string name,int price,short quantity)
+        {
+            productsTableAdapter.Insert(name, price, quantity);
+        }
+
+        public void UpdateProduct(int id,string name, int price, short quantity)
+        {
+            productsTableAdapter.Update(name, price, quantity,id);
+        }
+
+        public void DeleteProduct(int id)
+        {
+            productsTableAdapter.Delete(id);
+        }
+    }
+}
---
+++ TypedDataSetExample/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace TypedDataSetExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                CrudOperationsTypedDataSets crud = new CrudOperationsTypedDataSets();
+
+                int choice = DisplayMenu();
+
+                switch (choice)
+                {
+                    case 1: // Get All Products
+                        crud.GetAllProducts();
+                        break;
+
+                    case 2: // Get Product by ID
+                        Console.Write("\nEnter Product ID: ");
+                        int id = int.Parse(Console.ReadLine());
+                        crud.GetProductById(id);
+                        break;
+
+                    case 3: // Get Product by ID
+                        Console.Write("\nEnter Product ID: ");
+                        string _name = Console.ReadLine();
+                        crud.GetProductByName(_name);
+                        break;
+
+                    case 4: // Insert Product
+                        Console.Write("\nEnter Product Name: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Enter Product Price: ");
+                        int price = int.Parse(Console.ReadLine());
+                        Console.Write("Enter Product Quantity: ");
+                        short quantity = short.Parse(Console.ReadLine());
+                        crud.InsertProduct(name, price, quantity);
+                        break;
+
+                    case 5: // Update Product
+                        Console.Write("\nEnter Product Id: ");
+                        id = int.Parse(Console.ReadLine());
+                        crud.GetProductById(id);
+                        Console.Write("\nEnter Product Name: ");
+                        name = Console.ReadLine();
+                        Console.Write("Enter Product Price: ");
+                        price = int.Parse(Console.ReadLine());
+                        Console.Write("Enter Product Quantity: ");
+                        quantity = short.Parse(Console.ReadLine());
+                        crud.UpdateProduct(id, name, price, quantity);
+                        break;
+
+                    case 6: // Delete Product
+                        Console.Write("\nEnter Product Id: ");
+                        id = int.Parse(Console.ReadLine());
+                        crud.DeleteProduct(id);
+                        break;
+
+                    case 7: // Exit
+                        Environment.Exit(0);
+                        break;
+                }
+            } while (true);
+        }
+        static int DisplayMenu()
+        {
+            Console.WriteLine("\n\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+            Console.WriteLine("\t1 - Get all Products");
+            Console.WriteLine("\t2 - Get Product by ID");
+            Console.WriteLine("\t3 - Get Product by Name");
+            Console.WriteLine("\t4 - Insert Product");
+            Console.WriteLine("\t5 - Update Product");
+            Console.WriteLine("\t6 - Delete Product");
+            Console.WriteLine("\t7 - Exit");
+
+            Console.Write("\nEnter your choice: ");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
